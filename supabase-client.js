e941adf7d0b9dd5b3f965b651edd2a5ead43d532
Woomin-hub <woomin914@gmail.com>
2025-06-04 14:21:05 +0900

    update https://esm.sh/@supabase/supabase-js@2.39.5

--- supabase-client.js
+++ supabase-client.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js@2'
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2.39.5'
 import { SUPABASE_CONFIG } from './config.js'
 
 // Supabase 클라이언트 생성
